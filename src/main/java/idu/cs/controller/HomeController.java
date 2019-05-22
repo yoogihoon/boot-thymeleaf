27bcea483a1003209b6e437f2f482a27d08956d7
yoogihoon <rlgns987@naver.com>
2019-05-22 15:18:07 +0900

    author committer setting

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -19,8 +19,6 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home(Model model) {
-		model.addAttribute("test", "인덕 컴소");
-		model.addAttribute("egy", "유응구");
 		return "index";
 	}
 	@GetMapping("/register")
